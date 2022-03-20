c4a6004469f256e0e7bdfc13331f10810d41b9d6
aitomarabdeljalil <80470779+aitomarabdeljalil@users.noreply.github.com>
2022-03-20 20:48:00 +0100

    first commit

---
+++ minitalk/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 12:39:10 by aait-oma          #+#    #+#             */
+/*   Updated: 2022/03/19 21:20:05 by aait-oma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
